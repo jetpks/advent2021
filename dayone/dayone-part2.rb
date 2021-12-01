224b86b5a192b76b9060a60a6e71cecea5d1765d
Eric Jacobs <ejacobs@doximity.com>
2021-12-01 13:06:18 -0700

    revised dayone solution to be more better

--- dayone/dayone-part2.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-
-p File.readlines('./input')
-      .map { |v| v.chomp.to_i }
-      .each_cons(3)
-      .map(&:sum)
-      .each_cons(2)
-      .map { |s| s[1] - s[0] > 0 ? 1 : 0 }
-      .reduce(0, :+)
--- dayone/dayone.rb
+++ dayone/dayone.rb
@@ -1,7 +1,14 @@
 #!/usr/bin/env ruby
 
-p File.readlines('./input')
-      .map { |v| v.chomp.to_i }
-      .each_cons(2)
-      .map { |s| s[1] - s[0] > 0 ? 1 : 0 }
-      .reduce(0, :+)
+def input
+  File.readlines('./input').map { |v| v.chomp.to_i }
+end
+
+puts '// part one'
+p input.each_cons(2).count { |s| s[1] > s[0] }
+
+puts '// part two'
+p input.each_cons(3)
+       .map(&:sum)
+       .each_cons(2)
+       .count { |s| s[1] > s[0] }
